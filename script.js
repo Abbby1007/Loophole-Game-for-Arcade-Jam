fc4a9e5bb894e484f4df7ad01304fff627b98604
Abbby1007 <Abigailloken25@gmail.com>
2024-08-09 08:07:10 +0000

    More update for the first initial

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ function userName(){
 function yes(){
   document.getElementById('appear').innerHTML = `Well we want our employees to be able to think outside the box and solve problems. <br> Which is why  interviewees must solve 3 problems before moving on to the next interview phase 
   <br> You understand all that? 
-  <br> <a href="page3.html"> <button> Yes!!!!</button><a>  
+  <br> <button onclick="encourage()"> Yes </button> 
   <button onclick="no()"> NO?! </button>  `
 }
 
 function no(){
   document.getElementById('appear').innerHTML = ` Really ... dang, well ... GET OUT!!!! <br><a href="index.html"> <button> GET OUT !!!!</button> </a>`
+}
+
+function encourage(){
+  `oh and remmebr we encourage employees to dig around the rules <br>
+  <a href="page3.html"> <button> Next </button><a> `
 }
