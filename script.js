01bf998e7e7ae0b3bbf926b0beeccb398f1ff811
Abbby1007 <Abigailloken25@gmail.com>
2024-08-09 22:06:29 +0000

    Updating and storyline and adding the first challenge for the player

--- script.js
+++ script.js
@@ -9,17 +9,87 @@ function userName(){
   }
 
 function yes(){
-  document.getElementById('appear').innerHTML = `Well we want our employees to be able to think outside the box and solve problems. <br> Which is why  interviewees must solve 3 problems before moving on to the next interview phase 
-  <br> You understand all that? 
-  <br> <button onclick="encourage()"> Yes </button> 
+  document.getElementById('appear').innerHTML = `<b> Employer: </b> Well we want our employees to be able to think outside the box. <br> <b> Employer: </b> Which is why interviewees must complete 3 challenges before moving on to the next interview phase 
+  <br> <b> Employer: </b> You understand all that? 
+  <br> <button onclick="yesMoveOn()"> Yes!!! </button> 
   <button onclick="no()"> NO?! </button>  `
 }
 
 function no(){
-  document.getElementById('appear').innerHTML = ` Really ... dang, well ... GET OUT!!!! <br><a href="index.html"> <button> GET OUT !!!!</button> </a>`
+  document.getElementById('appear').innerHTML = ` <b> Employer: </b> Really ... dang, well ... GET OUT!!!! <br><a href="index.html"> <button> GET OUT !!!!</button> </a>`
 }
 
-function encourage(){
-  `oh and remmebr we encourage employees to dig around the rules <br>
-  <a href="page3.html"> <button> Next </button><a> `
+
+function yesMoveOn(){
+  document.getElementById('appear').innerHTML = `<b> Employer: </b> Great and I love the enthusiasm !!!! <br> <a href="page3.html"> <button> Next </button><a> `
 }
+  
+// Start of Rock Paper Scissors Code
+
+const rockButton = document.getElementById('ROCK');
+const paperButton = document.getElementById('PAPER');
+const scissorsButton = document.getElementById('SCISSORS');
+
+
+rockButton.addEventListener('click', () => playRound('ROCK'));
+paperButton.addEventListener('click', () => playRound('PAPER'));
+scissorsButton.addEventListener('click', () => playRound('SCISSORS'));
+
+let PlayerRoundWon = 0;
+let ComputerRoundWon = 0;
+// let roundDraws = 0;
+// let finalMessage = '';
+
+let currentRound = 1;
+const totalRound = 5;
+const roundDisplay = document.getElementById('round');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerRoundsWonDisplay = document.getElementById('player-rounds-won');
+const computerRoundsWonDisplay = document.getElementById('computer-rounds-won');
+const roundsDrawDisplay = document.getElementById(`round-draw`);
+// const finalMessageDisplay = document.getElementById('final-message');
+
+function playRound(playerChoice){
+     if (currentRound <= totalRound) {
+
+       if (currentRound == 3){
+         
+       }
+       roundDisplay.textContent = `Round: ${currentRound} of ${totalRound}`;
+       currentRound++;
+       if(playerChoice == 'ROCK'){
+         console.log(`player choose rock`);
+
+         playerChoiceDisplay.textContent = `Your choice: Rock`;
+         computerChoiceDisplay.textContent = `Computers choice: Paper`
+         ComputerRoundWon++;
+
+         playerRoundsWonDisplay.textContent = `Rounds Won: ${PlayerRoundWon}`;
+         computerRoundsWonDisplay.textContent = `Rounds Computer Won: ${ComputerRoundWon}`;
+       }
+         else if(playerChoice == 'PAPER'){
+           console.log(`player choose paper`)
+           playerChoiceDisplay.textContent = `Your choice: Paper`;
+           computerChoiceDisplay.textContent = `Computers choice: Scissors`
+           ComputerRoundWon++;
+
+           playerRoundsWonDisplay.textContent = `Rounds Won: ${PlayerRoundWon}`;
+           computerRoundsWonDisplay.textContent = `Rounds Computer Won: ${ComputerRoundWon}`;
+         }
+       else if(playerChoice == 'SCISSORS'){
+         console.log(`player choose Scissors`)
+
+         playerChoiceDisplay.textContent = `Your choice: Scissors`;
+         computerChoiceDisplay.textContent = `Computers choice: Rock`
+         ComputerRoundWon++;
+
+         playerRoundsWonDisplay.textContent = `Rounds Won: ${PlayerRoundWon}`;
+         computerRoundsWonDisplay.textContent = `Rounds Computer Won: ${ComputerRoundWon}`;
+       }
+     }
+  
+
+  
+}
+// End of Rock Paper Scissors Code
